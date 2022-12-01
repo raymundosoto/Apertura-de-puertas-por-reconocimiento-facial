93b2ff331ba3e847a0e411627714c8037ba2671c
M. En C. Raymundo Soto <raymundosotosoto@hotmail.com>
2022-12-01 13:56:49 -0600

    Update facial-attribute-analysis.py

--- facial-attribute-analysis.py
+++ facial-attribute-analysis.py
@@ -1,6 +1,6 @@
 from deepface import DeepFace
 
-obj = DeepFace.analyze (img_path = "/home/raymundo/Documents/apertura-puertas-reconocimiento-facial/image.jpeg", actions = ['age', 'gender', 'race', 'emotion'])
+obj = DeepFace.analyze (img_path = "/home/raymundo/Documents/apertura-puertas-reconocimiento-facial/image_2.jpg", actions = ['age', 'gender', 'race', 'emotion'])
 
 print ("El resultado del analisis es: ")
 print (obj)
